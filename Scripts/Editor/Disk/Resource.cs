6ae90d669cc9c0c34f0a9a6be6c31df45ac1c187
AntrelL <77912651+AntrelL@users.noreply.github.com>
2025-06-03 12:28:33 +0300

    feat(disk): add resource class

---
+++ Scripts/Editor/Disk/Resource.cs
@@ -0,0 +1,80 @@
+using Serein.Reflection;
+using System;
+using UnityEditor;
+
+namespace Serein.Disk.Editor
+{
+    public class Resource<T> where T : UnityEngine.Object
+    {
+        private static ConsoleOutputConfig s_consoleOutputConfig = 
+            new(Package.ModuleName.Disk, typeof(Resource<T>).GetCorrectGenericName());
+
+        private static Contract s_creationContract = new(
+            text: "Error creating asset, asset with this name already exists, path: ",
+            outputConfig: s_consoleOutputConfig);
+
+        private static Contract s_loadingContract = new(
+            text: "Failed to load asset at path ", 
+            outputConfig: s_consoleOutputConfig);
+
+        private T _asset;
+
+        private Resource(T asset, string path)
+        {
+            _asset = asset;
+            Path = path;
+        }
+
+        public string Path { get; private set; }
+
+        public static Resource<T> Create(string path, Func<T> assetCreator)
+        {
+            if (s_creationContract.CheckViolation(Exists(path), postfix: path))
+                return null;
+
+            T asset = assetCreator.Invoke();
+
+            AssetDatabase.CreateAsset(asset, path);
+            AssetDatabase.SaveAssets();
+
+            return new(asset, path);
+        }
+
+        public static Resource<T> Load(string path, Func<T> fallbackAssetCreator = null)
+        {
+            if (TryLoad(path, out Resource<T> resource))
+                return resource;
+
+            if (s_loadingContract.CheckViolation(fallbackAssetCreator is null, postfix: path))
+                return null;
+
+            return Create(path, fallbackAssetCreator);
+        }
+
+        public static bool TryLoad(string path, out Resource<T> resource)
+        {
+            T asset = AssetDatabase.LoadAssetAtPath<T>(path);
+
+            bool isSuccess = asset is not null;
+            resource = isSuccess ? new(asset, path) : null;
+
+            return isSuccess;
+        }
+
+        public static bool Exists(string path) => TryLoad(path, out _);
+
+        public void Edit(Action<T> changer, bool autoSave = true)
+        {
+            changer.Invoke(_asset);
+
+            if (autoSave)
+                Save();
+        }
+
+        public void Save()
+        {
+            EditorUtility.SetDirty(_asset);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
