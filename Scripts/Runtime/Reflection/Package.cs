47cb82641e843d740d754c00e482df05dda5109c
AntrelL <77912651+AntrelL@users.noreply.github.com>
2025-06-03 12:52:09 +0300

    fix(reflection): add missing module names

--- Scripts/Runtime/Reflection/Package.cs
+++ Scripts/Runtime/Reflection/Package.cs
@@ -5,12 +5,16 @@ namespace Serein.Reflection
         public const string Name = "Serein";
         public const string TechnicalName = "com.coldwind.serein";
         public const string Path = "Packages/" + TechnicalName;
+        public const string PathToDynamicDataFolder = Metadata.AssetRootFolderName + "/" + Name + "Data";
 
         public static class ModuleName
         {
             public const string Reflection = nameof(Reflection);
             public const string Debug = nameof(Debug);
             public const string Misc = nameof(Misc);
+
+            public const string Deployment = nameof(Deployment);
+            public const string Disk = nameof(Disk);
         }
     }
 }
