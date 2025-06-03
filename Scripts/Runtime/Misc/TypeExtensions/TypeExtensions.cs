3ca96e8fbdfcd4675f2b57175d8f388da54c53cf
AntrelL <77912651+AntrelL@users.noreply.github.com>
2025-06-03 12:26:42 +0300

    feat(misc): add extensions to type class

---
+++ Scripts/Runtime/Misc/TypeExtensions/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Serein
+{
+    public static class TypeExtensions
+    {
+        public static string GetCorrectGenericName(this Type type)
+        {
+            if (type.IsGenericType == false)
+                return type.Name;
+
+            string typeName = type.Name.Split('`')[0];
+            string genericArgs = string.Join(", ", type.GetGenericArguments().Select(t => t.Name));
+
+            return $"{typeName}<{genericArgs}>";
+        }
+    }
+}
