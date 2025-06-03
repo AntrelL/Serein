cab88814254196740bd789c3f3483e552924f5f8
AntrelL <77912651+AntrelL@users.noreply.github.com>
2025-06-03 15:47:05 +0300

    feat(reflection): add metadata class

---
+++ Scripts/Runtime/Reflection/Metadata.cs
@@ -0,0 +1,8 @@
+﻿namespace Serein.Reflection
+{
+    public class Metadata
+    {
+        public const string AssetRootFolderName = "Assets";
+        public const string GitKeepFileName = ".gitkeep";
+    }
+}
